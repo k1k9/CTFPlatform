d9d10cfa9909486b9ca2d51400943c649cd3c3d5
unknown <k1k9@protonmail.com>
2023-12-20 22:14:46 +0100

    Remove admin users from ranking

--- src/models/SolvesModel.php
+++ src/models/SolvesModel.php
@@ -18,15 +18,22 @@ class SolvesModel extends AbstractModel
          */
         $mysqli = $this->connectMysql();
         if ($mysqli) {
-            $stmt = $mysqli->prepare("SELECT Users.username, Solves.created_at FROM Solves INNER JOIN Users ON Solves.user_id = Users.id WHERE Solves.task_id = ? ORDER BY Solves.created_at ASC LIMIT ?");
-            $stmt->bind_param('ii', $task, $limit);
+            $stmt = $mysqli->prepare("SELECT Users.username, Users.permissions, Solves.created_at FROM Solves INNER JOIN Users ON Solves.user_id = Users.id WHERE Solves.task_id = ? ORDER BY Solves.created_at ASC");
+            $stmt->bind_param('i', $task);
             $stmt->execute();
             $result = $stmt->get_result();
             $solves = array();
     
             while ($row = $result->fetch_assoc()) {
-                $solves[] = $row;
+                if (count($solves) < $limit) {
+                    if($row['permissions'] != 2) {
+                        $solves[] = $row;
+                    }
+                } else {
+                    break;
+                }
             }
+
     
             $stmt->close();
             $mysqli->close();
